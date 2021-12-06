b1d037d3a7346cd61460ac8d77358cbccce25265
Jackson Davis <jackson@jcdav.is>
2021-12-06 07:56:23 -0800

    d6p2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,18 +4,16 @@ use std::fs;
 fn main() {
   let args: Vec<String> = env::args().collect();
   let input = fs::read_to_string(&args[1]).unwrap();
-  let mut fish: Vec<i32> = input.lines().next().expect("1 line").split(",").flat_map(|n| n.parse()).collect();
+  let fish: Vec<i32> = input.lines().next().expect("1 line").split(",").flat_map(|n| n.parse()).collect();
+  let mut states: Vec<i64> = vec![0,0,0,0,0,0,0,0,0];
 
-  for _ in 0..80 {
-    let end = fish.len();
-    for i in 0..end {
-      if fish[i] == 0 {
-        fish[i] = 6;
-        fish.push(8);
-      } else {
-        fish[i] -= 1;
-      }
-    }
+  for f in fish {
+    states[f as usize] += 1;
   }
-  println!("{}", fish.len());
+  for _ in 0..256 {
+    let zeros = states.remove(0);
+    states[6] += zeros;
+    states.push(zeros);
+  }
+  println!("{}", states.into_iter().sum::<i64>());
 }
