af4006d7d77bae5932077c732c91b0ba6453278d
Jackson Davis <jackson@jcdav.is>
2021-12-10 08:22:20 -0800

    d9p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,6 +2,26 @@ use std::env;
 use std::fs;
 
 use std::collections::HashMap;
+use std::collections::HashSet;
+
+fn recurse(map: &HashMap<i32, HashMap<i32, i32>>, seen: &mut HashSet<(i32, i32)>, i: i32, j: i32, prev: i32) {
+    if seen.contains(&(i,j)) {
+        return;
+    }
+    match map.get(&i).and_then(|row| row.get(&j)) {
+        Some(cell) => {
+            if *cell < 9 && *cell > prev {
+                seen.insert((i,j));
+                recurse(map, seen, i-1, j, *cell);
+                recurse(map, seen, i+1, j, *cell);
+                recurse(map, seen, i, j-1, *cell);
+                recurse(map, seen, i, j+1, *cell);
+            }
+        }
+        _ => {}
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input = fs::read_to_string(&args[1]).unwrap();
@@ -15,16 +35,20 @@ fn main() {
         (i as i32, line_map)
     }).collect();
 
-    let mut sum = 0;
+    let mut highest = vec!(0,0,0);
     for (i, row) in map.iter() {
         for (j, sq) in row.iter() {
             if sq < map.get(&(i-1)).and_then(|row| row.get(j)).unwrap_or(&10) &&
                 sq < map.get(&(i+1)).and_then(|row| row.get(j)).unwrap_or(&10) &&
                 sq < map.get(i).and_then(|row| row.get(&(j-1))).unwrap_or(&10) &&
                 sq < map.get(i).and_then(|row| row.get(&(j+1))).unwrap_or(&10) {
-                sum += sq+1;
+                let mut seen: HashSet<(i32, i32)> = HashSet::new();
+                recurse(&map, &mut seen, *i, *j, -1);
+                highest.push(seen.len());
+                highest.sort();
+                highest.remove(0);
             }
         }
     }
-    println!("{}", sum);
+    println!("{}", highest[0] * highest[1] * highest[2]);
 }
